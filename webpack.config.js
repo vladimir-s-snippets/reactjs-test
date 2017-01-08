716a096a05b8fb838f4e2e4baabff4eef7622426
Statsenko Vladimir <vovchiksta@gmail.com>
2017-01-08 22:37:16 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require("path");
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        publicPath: "/assets/",
+        path: './bin',
+        filename: 'app.bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+    devServer: {inline: true}
+};

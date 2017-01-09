ba954b48d79b783e802766c225578aa423ad2455
Vladimir Statsenko <vovchiksta@gmail.com>
2017-01-09 16:23:58 +0200

    components test

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function MyComponent(props) {
+    return <h2>Test {props.data}</h2>;
+}
+
+function App(props) {
+    return (
+        <div>
+            <MyComponent data="111" />
+            <MyComponent data="555" />
+            <MyComponent data="333" />
+            <div>{props.vvv}</div>
+        </div>
+    );
+}
+
 ReactDOM.render(
-    <h1>Hello, world!!!!!</h1>,
+    <App vvv="qqq" />,
     document.getElementById('root')
 );

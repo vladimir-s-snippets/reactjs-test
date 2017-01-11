38a9ec8044ddb746f2b69ec451d4a06ec7a8334d
Vladimir Statsenko <vovchiksta@gmail.com>
2017-01-11 15:49:44 +0200

    Events and conditional rendering

--- src/app.js
+++ src/app.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isToggledOn: false};
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            isToggledOn: !prevState.isToggledOn
+        }));
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleClick}>
+                    {this.state.isToggledOn ? 'ON' : 'OFF'}
+                </button>
+                <WarningBanner warn={this.state.isToggledOn} />
+            </div>
+        );
+    }
+}
+
 class Clock extends React.Component {
     constructor(props) {
         super(props);
@@ -30,7 +55,22 @@ class Clock extends React.Component {
     }
 }
 
+class WarningBanner extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        if (!this.props.warn) {
+            return null;
+        }
+        else {
+            return <div>Warning!</div>;
+        }
+    }
+}
+
 ReactDOM.render(
-    <Clock />,
+    <div><Clock /><Toggle /></div>,
     document.getElementById('root')
 );
